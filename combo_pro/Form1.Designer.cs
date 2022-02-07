6a13b517087146d28f7377e4af8a33db1d086e18
nirzaf <mfmfazrin1986@gmail.com>
2022-02-07 11:34:41 +0530

    Add project files.

---
+++ combo_pro/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace combo_pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.index_label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.itm_count_label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.search_textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Cat",
+            "Dog",
+            "Donkey",
+            "Zebra",
+            "Koala",
+            "Fish"});
+            this.comboBox1.Location = new System.Drawing.Point(151, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First Name:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Delete Selected";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // index_label2
+            // 
+            this.index_label2.AutoSize = true;
+            this.index_label2.Location = new System.Drawing.Point(297, 69);
+            this.index_label2.Name = "index_label2";
+            this.index_label2.Size = new System.Drawing.Size(35, 13);
+            this.index_label2.TabIndex = 5;
+            this.index_label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(257, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Items Count:";
+            // 
+            // itm_count_label3
+            // 
+            this.itm_count_label3.AutoSize = true;
+            this.itm_count_label3.Location = new System.Drawing.Point(132, 69);
+            this.itm_count_label3.Name = "itm_count_label3";
+            this.itm_count_label3.Size = new System.Drawing.Size(13, 13);
+            this.itm_count_label3.TabIndex = 8;
+            this.itm_count_label3.Text = "0";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Cat",
+            "Dog",
+            "Donkey",
+            "Zebra",
+            "Koala",
+            "Fish"});
+            this.comboBox2.Location = new System.Drawing.Point(151, 214);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(282, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Search By:";
+            // 
+            // search_textBox2
+            // 
+            this.search_textBox2.Location = new System.Drawing.Point(151, 183);
+            this.search_textBox2.Name = "search_textBox2";
+            this.search_textBox2.Size = new System.Drawing.Size(121, 20);
+            this.search_textBox2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 321);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.search_textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.itm_count_label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.index_label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label index_label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label itm_count_label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox search_textBox2;
+    }
+}
+
---
+++ combo_pro/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace combo_pro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+              string t;
+             t = this.textBox1.Text;
+            if (t=="")
+            {
+                MessageBox.Show("Hi;You can't enter empty name!!!");
+                return;
+            }
+            this.comboBox1.Items.Add(this.textBox1.Text );
+            this.itm_count_label3.Text = this.comboBox1.Items.Count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Int32 si;
+            si = this.comboBox1.SelectedIndex;
+
+            this.comboBox1.Items.RemoveAt(si);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.index_label2.Text = this.comboBox1.SelectedIndex.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.comboBox1.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.itm_count_label3.Text = this.comboBox1.Items.Count.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Int32 fi;
+           fi= this.comboBox2.FindStringExact(this.search_textBox2.Text);
+           
+           
+            if (fi == -1)
+
+            {
+                MessageBox.Show("Not Found!Try Again!");
+            }
+            else
+            {
+                this.Text = fi.ToString();
+               this.comboBox2.SelectedIndex = fi;
+            }
+        }
+    }
+}
